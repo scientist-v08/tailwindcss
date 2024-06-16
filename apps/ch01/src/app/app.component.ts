58b33cdb0b46d6360ed2e79f700d99a6e53ba520
scientist <venkimanoj2@gmail.com>
2024-06-16 21:25:36 +0530

    Initial commit

---
+++ apps/ch01/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'ch01';
+}
